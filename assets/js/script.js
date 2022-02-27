68faf42348704788df9b4a09449382b4b4557f16
Corey Motte <coreydavidmotte@gmail.com>
2022-02-27 15:15:49 -0500

    added end quiz logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,7 +29,6 @@ var questions = [
 // declare variables for timer interval and current question
 var score = 0;
 var timeLeft = 60;
-var timerInterval = 0;
 var currentQuestionPos = 0;
 
 var answerOne = document.getElementById("answerone");
@@ -38,20 +37,38 @@ var answerThree = document.getElementById("answerthree");
 var answerFour = document.getElementById("answerfour");
 var questionContainer = document.getElementById("questioncontainer");
 var questionContent = document.getElementById("title");
-
 var quizForm = document.getElementById("quizform");
+var timer = document.getElementById("timeremaining");
+var timeText = document.getElementById("timetext");
+
+// decrement timer every second and update text on timer span
+var handleTimer = setInterval(function () {
+    timeLeft--;
+    timer.textContent = timeLeft.toString();
+
+    if (timeLeft <= 0) {
+        endQuiz();
+        clearInterval(handleTimer);
+    }
+}, 1000);
 
 // function to generate question text and buttons
 function createQuestion() {
-    questionContent.textContent = questions[currentQuestionPos].text;
+    if (currentQuestionPos < questions.length) {
+        questionContent.textContent = questions[currentQuestionPos].text;
 
-    questions[currentQuestionPos].choices.map((answer) => {
-        var answerButton = document.createElement("button");
-        answerButton.textContent = answer;
-        quizForm.appendChild(answerButton);
-        answerButton.setAttribute("value", answer)
-        answerButton.addEventListener("click", answerCheck);
-    });  
+        questions[currentQuestionPos].choices.map((answer) => {
+            var answerButton = document.createElement("button");
+            answerButton.textContent = answer;
+            quizForm.appendChild(answerButton);
+            answerButton.setAttribute("value", answer);
+            answerButton.setAttribute("class", "btn btn-primary");
+            answerButton.addEventListener("click", answerCheck);
+        });  
+    } else {
+        endQuiz();
+    }
+    
 }
 
 // function to remove children of quizform once question is answered
@@ -67,16 +84,26 @@ function answerCheck(event) {
     var answer = event.target.value;
     if (answer === questions[currentQuestionPos].answer) {
         score += 25;
+        alert("Correct!");
     } else {
         timeLeft -= 10;
+        alert("Incorrect!");
     }
 
     currentQuestionPos++;
     clearQuestion();
     createQuestion();
-    
 }
 
-
+// function to end quiz, clear question/answers, and display time remaining and score
+function endQuiz(){
+    clearQuestion();
+    console.log("You did it idiot");
+    clearInterval(handleTimer);
+    timeText.textContent = "You finished with " + timeLeft.toString() + " seconds remaining.";
+    var completeText = document.createElement("h3");
+    completeText.textContent = "Congratulations! Your final score was: " + score.toString();
+    quizForm.appendChild(completeText);
+}
 
 createQuestion();
