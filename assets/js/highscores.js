23cbe47e9eef47035d86fd49aad6e22f530e220a
Corey Motte <coreydavidmotte@gmail.com>
2022-02-27 17:16:53 -0500

    fixed bug with table rows

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -3,7 +3,7 @@ var userScores = document.getElementById("scorelist");
 //function to create leaderboard on home page
 
 function createLeaderboard() {
-    // pull list of scores from localstorage and sort highest scores first
+    // pull list of scores from localstorage and sort highest scores
     var listOfScores = JSON.parse(localStorage.getItem('scoreList'));
     var sortedScores = listOfScores.sort(function (a, b) {
         return b.score - a.score;
@@ -11,17 +11,19 @@ function createLeaderboard() {
     
     // for loop to add each table element for high scores
     for (var i = 0; i < 10; i++) {
-        var tr = userScores.appendChild(document.createElement("tr"))
-        var th = document.createElement("th")
-        th.setAttribute("scope", "row");
-        th.textContent = i + 1;
-        tr.appendChild(th);
-        var name = document.createElement("td");
-        name.textContent = sortedScores[i].name;
-        tr.appendChild(name);
-        var score = document.createElement("td");
-        score.textContent = sortedScores[i].score;
-        tr.appendChild(score);
+        if (sortedScores[i]){
+            var tr = userScores.appendChild(document.createElement("tr"))
+            var th = document.createElement("th")
+            th.setAttribute("scope", "row");
+            th.textContent = i + 1;
+            tr.appendChild(th);
+            var name = document.createElement("td");
+            name.textContent = sortedScores[i].name;
+            tr.appendChild(name);
+            var score = document.createElement("td");
+            score.textContent = sortedScores[i].score;
+            tr.appendChild(score);
+        } 
     }
 }
 
