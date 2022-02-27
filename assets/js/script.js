464e7c583bedd1fce041ff008d1a8d48e1dc547f
Corey Motte <coreydavidmotte@gmail.com>
2022-02-27 13:04:46 -0500

    added landing page and quiz page

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+var questions = [
+    {
+        text: "Insert question 1 here",
+        choices: ["option", "option", "option", "option"],
+        answer: "option"
+    },
+
+    {
+        text: "Insert question 2 here",
+        choices: ["option", "option", "option", "option"],
+        answer: "option"
+    },
+
+    {
+        text: "Insert question 3 here",
+        choices: ["option", "option", "option", "option"],
+        answer: "option"
+    },
+
+    {
+        text: "Insert question 4 here",
+        choices: ["option", "option", "option", "option"],
+        answer: "option"
+    },
+]
+
+var score = 0;
+
+var button1;
+var button2;
+var button3;
+var button4;
+
+var questionsEl = document.querySelector(#question);
+var highscoresEl = document.querySelector(#highscores);
